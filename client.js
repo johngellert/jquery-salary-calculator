4e1974a8fc11cd5d822024b4374cf316ccc80c84
John Gellert <gellertjm@gmail.com>
2019-05-19 07:54:41 -0500

    base mode functional

--- client.js
+++ client.js
@@ -2,9 +2,13 @@ console.log('js load');
 
 $(document).ready(readyNow);
 
+let totalMonthlyCost = 0;
+
 function readyNow() {
     console.log('jq load');
     $('.submitButton').on('click', handleSubmitClick);
+    $('.employeeTable').on('click', '.deleteButton', handleDeleteButton);
+    updateTotalMonthly(totalMonthlyCost);
 }
 
 function handleSubmitClick() {
@@ -14,25 +18,44 @@ function handleSubmitClick() {
     const title = $('.title').val();
     const annualSalary = $('.annualSalary').val();
 
-    appendToEmployeeTable(firstName, lastName, id, title, annualSalary);
+    addToEmployeeTable(firstName, lastName, id, title, annualSalary);
+    
+    // $('.firstName').val('');
+    // $('.lastName').val('');
+    // $('.id').val('');
+    // $('.title').val('');
+    // $('.annualSalary').val('');
+    
+    totalMonthlyCost += parseInt(annualSalary);
 
-    $('.firstName').val('');
-    $('.lastName').val('');
-    $('.id').val('');
-    $('.title').val('');
-    $('.annualSalary').val('');
+    updateTotalMonthly(totalMonthlyCost);
 
     console.log('hello');
 }
 
-function appendToEmployeeTable(firstOut, lastOut, idOut, titleOut, salaryOut) {
+function addToEmployeeTable(firstOut, lastOut, idOut, titleOut, salaryOut) {
     $('.employeeTable').append(`
     <tr>
         <td>${firstOut}</td>
         <td>${lastOut}</td>
         <td>${idOut}</td>
         <td>${titleOut}</td>
-        <td>${salaryOut}</td>
+        <td id="salaryColumn">${salaryOut}</td>
+        <td id="deleteColumn">
+            <button class="deleteButton">Delete</button>
+        </td>
     </tr>
     `);
+}
+
+function handleDeleteButton() {
+    $(this).closest('tr').remove();
+    console.log('delete');
+}
+
+function updateTotalMonthly(totalMonthlyCostOut){
+   if(totalMonthlyCost > 20000){
+       $('#monthlyCost').css('background-color', 'red');
+   } 
+    $('#monthlyCost').text(totalMonthlyCostOut);
 }
