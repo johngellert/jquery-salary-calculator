f32640cd818429e5b5d3450685ee0618d11ca94e
John Gellert <gellertjm@gmail.com>
2019-05-18 19:38:01 -0500

    inputs are outputting to the DOM and input fields are clearing when clicking submit button

--- client.js
+++ client.js
@@ -2,7 +2,37 @@ console.log('js load');
 
 $(document).ready(readyNow);
 
-function readyNow(){
+function readyNow() {
     console.log('jq load');
+    $('.submitButton').on('click', handleSubmitClick);
+}
 
+function handleSubmitClick() {
+    const firstName = $('.firstName').val();
+    const lastName = $('.lastName').val();
+    const id = $('.id').val();
+    const title = $('.title').val();
+    const annualSalary = $('.annualSalary').val();
+
+    appendToEmployeeTable(firstName, lastName, id, title, annualSalary);
+
+    $('.firstName').val('');
+    $('.lastName').val('');
+    $('.id').val('');
+    $('.title').val('');
+    $('.annualSalary').val('');
+
+    console.log('hello');
+}
+
+function appendToEmployeeTable(firstOut, lastOut, idOut, titleOut, salaryOut) {
+    $('.employeeTable').append(`
+    <tr>
+        <td>${firstOut}</td>
+        <td>${lastOut}</td>
+        <td>${idOut}</td>
+        <td>${titleOut}</td>
+        <td>${salaryOut}</td>
+    </tr>
+    `);
 }
