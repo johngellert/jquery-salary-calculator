ab9ddb3da7914ebb72f9ce0ebe478cae142fdc68
John Gellert <gellertjm@gmail.com>
2019-05-20 09:03:55 -0500

    added code comments. base mode completed.

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ $(document).ready(readyNow);
 
 let totalMonthlyCost = 0;
 
+
 function readyNow() {
     console.log('jq load');
     $('.submitButton').on('click', handleSubmitClick);
@@ -11,35 +12,46 @@ function readyNow() {
     updateTotalMonthly(totalMonthlyCost);
 }
 
+// delcare handleSubmitClick
+// captures input from the DOM and adds inputs to the employee table on the DOM
 function handleSubmitClick() {
+    // declare variables to store input data
     const firstName = $('.firstName').val();
     const lastName = $('.lastName').val();
     const id = $('.id').val();
     const title = $('.title').val();
     const annualSalary = $('.annualSalary').val();
 
+    // clear input data
     $('.firstName').val('');
     $('.lastName').val('');
     $('.id').val('');
     $('.title').val('');
     $('.annualSalary').val('');
 
+    // checks that all fields were completed before adding input data to the employee table
     if(validateInputData(firstName, lastName, id, title, annualSalary)) {
-        return;
+        return; // ends if a field is empty
     }
 
+    // formatting the annual salary stored in table
     let annualSalarayFormated = parseInt(annualSalary);
     annualSalarayFormated = annualSalarayFormated.toLocaleString();
 
+    // adds input data to employee table by calling addToEmployeeTable
     addToEmployeeTable(firstName, lastName, id, title, annualSalarayFormated);
 
+    // converts annual salary to monthly salary and adds to monthly cost
     totalMonthlyCost += parseInt(annualSalary) / 12;
 
+    // updates monthly cost
     updateTotalMonthly(totalMonthlyCost);
 
-    console.log('hello');
+    //console.log('hello');
 }
 
+// declare addToEmpllyeeTable
+// accepts five parameters and appends the data to the employee table on the DOM
 function addToEmployeeTable(firstOut, lastOut, idOut, titleOut, salaryOut) {
     $('.employeeTable').append(`
     <tr>
@@ -55,11 +67,15 @@ function addToEmployeeTable(firstOut, lastOut, idOut, titleOut, salaryOut) {
     `);
 }
 
+// declare handleDeleteButton
+// removes an employee from the table on the DOM
 function handleDeleteButton() {
     $(this).closest('tr').remove();
-    console.log(deleteMonthlyCost);
 }
 
+// declare updateTotalMonthly
+// takes one parameter
+// updates the total monthly cost and changes the css color if over 20,000
 function updateTotalMonthly(totalMonthlyCostOut) {
     if (totalMonthlyCost > 20000) {
         $('#monthlyCost').css('background-color', 'red');
@@ -68,6 +84,7 @@ function updateTotalMonthly(totalMonthlyCostOut) {
         $('#monthlyCost').css('background-color', 'transparent');
     }
 
+    // formatting the monthly cost
     totalMonthlyCostOut = totalMonthlyCostOut.toFixed(2);
     totalMonthlyCostOut = parseFloat(totalMonthlyCostOut);
     totalMonthlyCostOut = totalMonthlyCostOut.toLocaleString();
@@ -75,6 +92,8 @@ function updateTotalMonthly(totalMonthlyCostOut) {
     $('#monthlyCost').text(totalMonthlyCostOut);
 }
 
+// declare validateInputData
+// checks that the fields are not empty
 function validateInputData(first, last, id, title, salary) {
     if (!first) {
         alert("Please complete all fields");
